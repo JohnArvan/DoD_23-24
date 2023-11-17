dec7afb0c5bb53600e37e22fb6e53bdfd7ccc743
JohnArvan <jjarvan2002@gmail.com>
2023-11-16 20:54:00 -0500

    Farmer moves toward player

--- Source/Gameplay/FarmerSubsection/Farm.cs
+++ Source/Gameplay/FarmerSubsection/Farm.cs
@@ -15,8 +15,12 @@ namespace DoD_23_24
         FarmPlayer playerInstance;
         FarmPlayer bigBroInstance;
         FarmPlayer lilBroInstance;
+
+        FarmPlayer currentPlayerInstance;
         Farmer farmer;
         private bool switchingPlayer = true;
+
+        public static Vector2 farmerTrackPos;
 
         public Farm()
         {
@@ -30,7 +34,9 @@ namespace DoD_23_24
             lilBroInstance = new FarmPlayer("2D/Sprites/Item", new Vector2(120, 100), new Vector2(12, 12), true, level);
             lilBroInstance.ChangeSpeed(1.25f);
 
-            farmer = new Farmer("2D/Sprites/Item", new Vector2(50, 150), new Vector2(16, 16), true);
+            currentPlayerInstance = playerInstance;
+
+            farmer = new Farmer("2D/Sprites/Item", new Vector2(50, 150), new Vector2(16, 16), true, level);
         }
 
         public void Update(GameTime gameTime)
@@ -41,6 +47,8 @@ namespace DoD_23_24
             farmer.Update(gameTime);
 
             SwitchPlayer();
+
+            farmerTrackPos = currentPlayerInstance.pos;
         }
 
         public void Draw()
@@ -59,20 +67,29 @@ namespace DoD_23_24
             if (kstate.IsKeyDown(Keys.Space) && switchingPlayer)
             {
                 switchingPlayer = !switchingPlayer;
+                //Switch to big brother
                 if (playerInstance.CheckCurrentPlayer())
                 {
                     playerInstance.ChangeCurrentPlayer();
                     bigBroInstance.ChangeCurrentPlayer();
+
+                    currentPlayerInstance = bigBroInstance;
                 }
+                //Switch to little brother
                 else if (bigBroInstance.CheckCurrentPlayer())
                 {
                     bigBroInstance.ChangeCurrentPlayer();
                     lilBroInstance.ChangeCurrentPlayer();
+
+                    currentPlayerInstance = lilBroInstance;
                 }
+                //Switch to player
                 else
                 {
                     lilBroInstance.ChangeCurrentPlayer();
                     playerInstance.ChangeCurrentPlayer();
+
+                    currentPlayerInstance = playerInstance;
                 }
             }
             else if (kstate.IsKeyUp(Keys.Space) && !switchingPlayer)
--- Source/Gameplay/FarmerSubsection/Farmer.cs
+++ Source/Gameplay/FarmerSubsection/Farmer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +11,27 @@ namespace DoD_23_24
     public class Farmer : Basic2D
     {
         public float speed = 25f;
+        public Rectangle farmerBounds;
         private Vector2 trackPos;
+        Level level;
 
-        public Farmer(string PATH, Vector2 POS, Vector2 DIMS, bool shouldScale) : base(PATH, POS, DIMS, shouldScale)
+        public Farmer(string PATH, Vector2 POS, Vector2 DIMS, bool shouldScale, Level level) : base(PATH, POS, DIMS, shouldScale)
         {
-            trackPos = new Vector2(100, 100);
+            farmerBounds = new Rectangle((int)pos.X - (int)(dims.X / 2), (int)pos.Y - (int)(dims.Y / 2), (int)dims.X, (int)dims.Y);
+            this.level = level;
         }
 
         public override void Update(GameTime gameTime)
         {
+            Move(gameTime);
             base.Update(gameTime);
+        }
+
+        //Move towards current target
+        public void Move(GameTime gameTime)
+        {
+            trackPos = Farm.farmerTrackPos;
+            Vector2 initPos = pos;
 
             if (pos.X < trackPos.X - 0.5f)
             {
@@ -29,6 +41,8 @@ namespace DoD_23_24
             {
                 pos.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
+            farmerBounds.X = (int)pos.X - (int)(dims.X / 2);
+            if (level.CheckCollision(farmerBounds)) pos.X = initPos.X;
 
             if (pos.Y < trackPos.Y - 0.5f)
             {
@@ -38,6 +52,8 @@ namespace DoD_23_24
             {
                 pos.Y -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
+            farmerBounds.Y = (int)pos.Y - (int)(dims.Y / 2);
+            if (level.CheckCollision(farmerBounds)) pos.Y = initPos.Y;
         }
     }
 }
