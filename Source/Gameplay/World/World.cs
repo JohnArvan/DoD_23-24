8f8ad4b9b11aaf2e73fcf0f798d300204a98e35e
JohnArvan <jjarvan2002@gmail.com>
2023-11-18 18:07:44 -0500

    Fixed merge problems

--- Source/Gameplay/World/World.cs
+++ Source/Gameplay/World/World.cs
@@ -21,28 +21,33 @@ namespace DoD_23_24
 {
     public class World
     {
-        Level level;
-        Player playerInstance;
-        NPC book;
+
+        //Level level;
+        //Player playerInstance;
+
+        Farm farm;
 
         public World()
         {
-            level = new Level("Content/map.tmx", "Tiny Adventure Pack\\");
-            playerInstance = new Player("2D/Sprites/Item", new Vector2(100, 100), new Vector2(16, 16), true, level);
-            book = new NPC("2D/Sprites/Special1", new Vector2(100, 200), new Vector2(16, 16), true, "C:\\Users\\User\\Documents\\GitHub\\DoD_23-24\\Content\\NPCText\\TestNPC.txt", playerInstance);
+            //level = new Level("Content/map.tmx", "Tiny Adventure Pack\\");
+            //playerInstance = new Player("2D/Sprites/Item", new Vector2(100, 100), new Vector2(16, 16), true, level);
+
+            farm = new Farm();
         }
 
         public virtual void Update(GameTime gameTime)
         {
-            playerInstance.Update(gameTime);
-            book.Update(gameTime);
+            //playerInstance.Update(gameTime);
+
+            farm.Update(gameTime);
         }
 
         public virtual void Draw()
         {
-            level.Draw();
-            playerInstance.Draw();
-            book.Draw();
+            //level.Draw();
+            //playerInstance.Draw();
+
+            farm.Draw();
         }
 
         public Player GetPlayer()
@@ -52,3 +57,4 @@ namespace DoD_23_24
         }
     }
 }
+
